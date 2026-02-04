1c56b589808f55dac521bb685a0ad2992cfb487d
Zachary Ables <zach.ables.ctr@tri.global>
2026-02-04 11:21:50 -0800

    stack select

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,7 @@ for i in $WORKDIRS; do
   options="--yes --non-interactive --color=never"
 
   cd $i
+  pulumi stack select main || pulumi stack init main 
   echo Deploying for $i
   if [ "$UPDATE_STATE" == "true" ]; then
     echo "Updating state for $i"
