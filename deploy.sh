7c0f59e126f8d6cc800719fd0133f0dbc8f6f86e
Zachary Ables <zach.ables.ctr@tri.global>
2026-02-09 11:29:34 -0800

    plan

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 export CI=1
 
+REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
+
+pulumi login s3://tri-pulumi-state-us-east-1/$REPO_NAME
+
 for i in $WORKDIRS; do
   if [ ! -d $i ]; then
     echo $i is not a directory, skipping..
--- plan.sh
+++ plan.sh
@@ -37,7 +37,9 @@ plan() {
     cd ..
   fi
 }
+
 REPO_NAME=$(echo $GITHUB_REPOSITORY | cut -d'/' -f2)
+
 pulumi login s3://tri-pulumi-state-us-east-1/$REPO_NAME
 
 for i in $WORKDIRS; do
