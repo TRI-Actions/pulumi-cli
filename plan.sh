b94d6c237783ef747067053b5225d19f785b7897
Zachary Ables <zach.ables.ctr@tri.global>
2026-02-09 11:35:16 -0800

    main

--- plan.sh
+++ plan.sh
@@ -4,7 +4,7 @@ export CI=1
 plan() {
   dir=$1
   cd $dir
-  pulumi stack select $dir || pulumi stack init $dir
+  pulumi stack select main || pulumi stack init main
 
   echo Running plan for $dir
   options=" --color=never --diff"
