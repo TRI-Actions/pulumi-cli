9ad641b4f4d717cabe381fb5f1b920e20a70e14e
Zachary Ables <zach.ables.ctr@tri.global>
2026-02-09 09:26:57 -0800

    -v

--- plan.sh
+++ plan.sh
@@ -7,7 +7,7 @@ plan() {
   pulumi stack select $dir || pulumi stack init $dir
 
   echo Running plan for $dir
-  options=" --color=never --diff"
+  options=" --color=never -v=9"
   if [ "$DRIFT_CHECK" == "true" ]; then
 
     pulumi refresh --yes
