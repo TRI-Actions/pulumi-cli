f6e698cc3fa375d874f874b9da48ad7b6aee54ec
zables1 <143547880+zables1@users.noreply.github.com>
2026-03-05 08:45:44 -0800

    Update plan.sh

--- plan.sh
+++ plan.sh
@@ -4,7 +4,7 @@ export CI=1
 plan() {
   dir=$1
   cd $dir
-  pulumi stack select main || pulumi stack init main
+  pulumi stack select main || pulumi stack init main --secrets-provider=awskms://alias/pulumi-stack-key
 
   echo Running plan for $dir
   options=" --color=never --diff"
